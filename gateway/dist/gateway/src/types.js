e8ccd6a69fc478f478ec98d0855fa7cfd6ba5304
Jesper R. Goksør <jesperrg@stud.ntnu.no>
2025-12-10 07:30:39 +0100

    Delete gateway/dist directory

--- gateway/dist/gateway/src/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// gateway/src/types.ts
-// Local re-export of shared API types, so gateway code only imports from "./types"
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
--- gateway/dist/shared/apiTypes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// shared/apiTypes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=apiTypes.js.map
